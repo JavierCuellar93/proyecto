b571657d82fefca10fd41185d8006bc557d5da45
JavierCuellar <javier.cuellar.sandoval@gmail.com>
2025-03-05 12:18:32 -0600

    Definicion de componentes

---
+++ src/app/components/location-forecast/location-forecast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-forecast',
+  imports: [],
+  templateUrl: './location-forecast.component.html',
+  styleUrl: './location-forecast.component.css'
+})
+export class LocationForecastComponent {
+
+}
