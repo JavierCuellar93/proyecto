8bb9394b2d6612c5c6cd0cf58ae7d0b8477753a1
JavierCuellar <javier.cuellar.sandoval@gmail.com>
2025-03-07 23:55:15 -0600

    Funcionamiento principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocationFormComponent } from "./components/location-form/location-form.component";
+import { LocationWeatherComponent } from "./components/location-weather/location-weather.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LocationFormComponent, LocationWeatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto';
+  locationZipCode!: string;
+
+  receiveZipCode(zipCode: string) {
+    this.locationZipCode = zipCode;
+  }
+
 }
--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServiceService {
 
-  constructor() { }
+  private googleKey ='&key=AIzaSyBzB9hhI7-oU9SbkUi7K3kQfer3_vPJucA';
+  private googleGeoURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+  
+  private openWeatherAPIURL = 'https://api.openweathermap.org/data/3.0/onecall?';
+  private openWeatherKey = '&appid=5a4b2d457ecbef9eb2a71e480b947604';
+  private lat = 'lat=';
+  private lon = '&lon=';
+  private excludeAllButCurrent = '&exclude=minutely,hourly,daily,alerts';
+
+  private baseImageURL = 'https://www.angulartraining.com/images/weather/';
+  private imageURL = '';
+  
+  constructor(private httpClient: HttpClient) { 
+
+  }
+
+  getLocationData(zipCode: string): Observable<any> {
+    return this.httpClient.get(this.googleGeoURL + zipCode + this.googleKey);
+  }
+
+  getWeatherData(latValue: string, lonValue: string): Observable<any> {
+    const request = this.openWeatherAPIURL + this.lat + latValue + this.lon + lonValue + this.excludeAllButCurrent + this.openWeatherKey;
+    console.log('REQUEST: ', request);
+    return this.httpClient.get(request);
+  }
+
+  getWeatherImage(weather: string) {
+    switch(weather) {
+      case 'sunny': {
+        this.imageURL = this.baseImageURL + 'sun.png';
+        break;
+      }
+      case 'snowy': {
+        this.imageURL = this.baseImageURL + 'snow.png';
+        break;
+      }
+      case 'cloudy': {
+        this.imageURL = this.baseImageURL + 'clouds.png';
+        break;
+      }
+      case 'rainy': {
+        this.imageURL = this.baseImageURL + 'rain.png';
+        break;
+      }
+    }
+    return this.imageURL;
+  }
+
 }
